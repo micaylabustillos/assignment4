daaf79b2fbdf54b24caed76433a2432729f3b9fc
Micayla Bustillos <micaylabustillos@Micaylas-MacBook-Air.local>
2026-04-06 22:56:34 -0500

    app done

--- application.py
+++ application.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 def home():
     # Requirement 1
     # read env var MY_ENV_VAR
+    value = os.getenv("MY_ENV_VAR", "Not set")
     # return its value
-    return ""
+    return value
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
